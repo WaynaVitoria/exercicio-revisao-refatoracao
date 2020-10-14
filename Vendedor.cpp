4f16998509ae064f8d4d7763f288ae80f2a38506
WaynaVitoria <72826012+WaynaVitoria@users.noreply.github.com>
2020-10-14 15:42:59 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -2,12 +2,13 @@
 #include "Empregado.hpp"
 
 class Vendedor : public Empregado {
-     	  
+  private:
+    double quotaMensalVendas; 
   public:	
     double quotaTotalAnual() {
-    	const double meses= 12;
-    	double pagamentoTotal= quotaMensalVendas * meses;	    
-    	return pagamentoTotal;
+    	 const double meses= 12;
+    	 double pagamentoTotal= quotaMensalVendas * meses;	    
+    	 return pagamentoTotal;
     }	
 };
 
